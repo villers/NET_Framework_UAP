42bd6cc3d72c52f793d4c292ad5730a8c260045a
VILLERS Mickaël <mickael.villers@epitech.eu>
2016-01-26 23:04:08 +0100

    Update Windows Application

--- NET_Framework/NET_Framework.Windows/MainPage.xaml.cs
+++ NET_Framework/NET_Framework.Windows/MainPage.xaml.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using NET_Framework.DataModel;
+using NET_Framework.Model;
+using System;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +20,22 @@ namespace NET_Framework
         public MainPage()
         {
             this.InitializeComponent();
+        }   
+
+        /// <summary>
+        /// Invoqué lorsque cette page est sur le point d'être affichée dans un frame.
+        /// </summary>
+        /// <param name="e">Données d'événement décrivant la manière dont l'utilisateur a accédé à cette page.
+        /// Ce paramètre est généralement utilisé pour configurer la page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: préparer la page pour affichage ici.
+
+            // TODO: si votre application comporte plusieurs pages, assurez-vous que vous
+            // gérez le bouton Retour physique en vous inscrivant à l’événement
+            // Événement Windows.Phone.UI.Input.HardwareButtons.BackPressed.
+            // Si vous utilisez le NavigationHelper fourni par certains modèles,
+            // cet événement est géré automatiquement.
         }
     }
 }
